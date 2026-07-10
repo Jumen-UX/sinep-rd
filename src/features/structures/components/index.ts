854677c143c39833d97626a19f7a1bca6c5d39ab
Jumen-UX <juan_mendoza@outlook.com>
2026-07-10 14:43:44 -0400

    refactor(structures): export node detail panel

--- src/features/structures/components/index.ts
+++ src/features/structures/components/index.ts
@@ -1,4 +1,5 @@
 export * from './StructureLevelEditor'
+export * from './StructureNodeDetailPanel'
 export * from './StructureNodeEditor'
 export * from './StructurePresetGrid'
 export * from './StructureSummary'
