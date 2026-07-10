54c8722cbb62880e1538e1beb4499a285f633850
Jumen-UX <juan_mendoza@outlook.com>
2026-07-10 16:27:43 -0400

    security(deacon): grant scoped reader execution chain

---
+++ supabase/migrations/20260710202717_grant_scoped_unordained_reader_chain.sql
@@ -0,0 +1,2 @@
+grant execute on function app_private.current_user_can_manage_person(text, uuid) to authenticated;
+grant execute on function app_private.admin_list_unordained_people(integer) to authenticated;
