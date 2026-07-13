23189dbec5305e07dda3685f6dca3b1129902080
Jumen-UX <juan_mendoza@outlook.com>
2026-07-13 12:24:42 -0400

    refactor(entities): keep public detail on one data request

--- src/app/(public)/entidades/[slug]/page.tsx
+++ src/app/(public)/entidades/[slug]/page.tsx
@@ -1,17 +1,7 @@
 'use client'
 
-import { useParams } from 'next/navigation'
 import { EntityDetailPageView } from '@/features/entidades/EntityDetailPage'
-import EntityInstitutionalTimeline from '@/features/entidades/EntityInstitutionalTimeline'
 
 export default function EntityDetailPage() {
-  const params = useParams<{ slug: string }>()
-  const slug = params?.slug
-
-  return (
-    <>
-      <EntityDetailPageView />
-      {slug && <EntityInstitutionalTimeline slug={slug} />}
-    </>
-  )
+  return <EntityDetailPageView />
 }
