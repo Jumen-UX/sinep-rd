109f42d6634a815228e1bbcc01fe43f3c9add100
Jumen-UX <juan_mendoza@outlook.com>
2026-07-13 15:13:59 -0400

    refactor(db): eliminar plantillas estructurales heredadas

---
+++ supabase/migrations/20260713235500_remove_obsolete_structure_template_tables.sql
@@ -0,0 +1,4 @@
+drop table if exists public.pastoral_structure_levels;
+drop table if exists public.pastoral_structure_templates;
+drop table if exists public.diocese_structure_levels;
+drop table if exists public.diocese_structure_templates;
