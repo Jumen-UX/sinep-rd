b93829244b9cd7a40fdaf73c06daa292cd4597bf
Jumen-UX <juan_mendoza@outlook.com>
2026-07-14 15:23:10 -0400

    refactor(access): export authentication pages and service

--- src/features/access/index.ts
+++ src/features/access/index.ts
@@ -1,3 +1,6 @@
-export { default as UserAccessPage } from './admin/UserAccessPage'
+export { default as AdminLoginPage } from './admin/AdminLoginPage'
 export { default as InviteUserPage } from './admin/InviteUserPage'
+export { default as ResetAccessPage } from './admin/ResetAccessPage'
+export { default as UserAccessPage } from './admin/UserAccessPage'
+export * from './services/authentication-admin-service'
 export * from './services/user-access-admin-service'
