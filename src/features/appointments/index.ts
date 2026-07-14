a8edcbd759aaa2c6d3886f602890dd74f8f21663
Jumen-UX <juan_mendoza@outlook.com>
2026-07-14 10:41:45 -0400

    refactor(appointments): export office configuration feature

--- src/features/appointments/index.ts
+++ src/features/appointments/index.ts
@@ -1,2 +1,4 @@
 export { default as AssignmentManagerPage } from './admin/AssignmentManagerPage'
+export { default as OfficeConfigurationPage } from './admin/OfficeConfigurationPage'
 export * from './services/assignment-admin-service'
+export * from './services/office-configuration-admin-service'
