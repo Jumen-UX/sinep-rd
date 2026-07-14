fe613ca5a8821b9cd134d6c6526c5408df6f8b44
Jumen-UX <juan_mendoza@outlook.com>
2026-07-14 10:51:40 -0400

    refactor(access): export access administration feature

---
+++ src/features/access/index.ts
@@ -0,0 +1,3 @@
+export { default as UserAccessPage } from './admin/UserAccessPage'
+export { default as InviteUserPage } from './admin/InviteUserPage'
+export * from './services/user-access-admin-service'
