0c4b059ee55bc32a0b9c59081dc9c862fed143af
Jumen-UX <juan_mendoza@outlook.com>
2026-07-14 11:20:07 -0400

    refactor(events): export event registry feature

--- src/features/events/index.ts
+++ src/features/events/index.ts
@@ -1,2 +1,4 @@
 export { default as EventDraftPage } from './admin/EventDraftPage'
+export { default as EventRegistryPage } from './admin/EventRegistryPage'
 export * from './services/event-draft-admin-service'
+export * from './services/event-registry-admin-service'
