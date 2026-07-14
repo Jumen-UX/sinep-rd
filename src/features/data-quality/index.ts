d582d433f409966fc41b76b352aa69c875b3b612
Jumen-UX <juan_mendoza@outlook.com>
2026-07-14 13:04:34 -0400

    refactor(data-quality): export administration service

--- src/features/data-quality/index.ts
+++ src/features/data-quality/index.ts
@@ -1,3 +1,4 @@
 export { default as JurisdictionAlertsPage } from './admin/JurisdictionAlertsPage'
 export { default as RecordCompletenessPage } from './admin/RecordCompletenessPage'
 export { default as StructureAlertsPage } from './admin/StructureAlertsPage'
+export * from './services/data-quality-admin-service'
