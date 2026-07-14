4fa48582344b53d2f608df5110625469ce32c06e
Jumen-UX <juan_mendoza@outlook.com>
2026-07-14 12:33:53 -0400

    refactor(entities): export entity creation feature

---
+++ src/features/entities/index.ts
@@ -0,0 +1,3 @@
+export { default as NewChapelPage } from './admin/NewChapelPage'
+export { default as NewJurisdictionPage } from './admin/NewJurisdictionPage'
+export { default as NewParishPage } from './admin/NewParishPage'
