5c5fe10e19c7450dd9e07679f30dd3cf39007376
Jumen-UX <juan_mendoza@outlook.com>
2026-07-18 12:05:23 -0400

    Block crawlers until public indexing is enabled

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,8 +1,19 @@
 import type { MetadataRoute } from 'next'
 import { getAppBaseUrl } from '@/lib/appBaseUrl'
+import { isPublicIndexingEnabled } from '@/lib/public/indexing'
 
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = getAppBaseUrl()
+
+  if (!isPublicIndexingEnabled()) {
+    return {
+      rules: {
+        userAgent: '*',
+        disallow: '/',
+      },
+      host: baseUrl,
+    }
+  }
 
   return {
     rules: {
