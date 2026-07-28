d29fb7e65891876e59964488df9d6d2c3c5bf7ff
Jumen-UX <juan_mendoza@outlook.com>
2026-07-28 18:50:10 -0400

    feat(registry): export edit history feature

--- src/features/ecclesial-registry/index.ts
+++ src/features/ecclesial-registry/index.ts
@@ -1,2 +1,4 @@
 export { default as EcclesialRegistryPage } from './admin/EcclesialRegistryPage'
+export { default as EcclesialRegistryHistoryPage } from './admin/EcclesialRegistryHistoryPage'
 export * from './services/ecclesial-registry-admin-service'
+export * from './services/ecclesial-registry-history-service'
