bf965c0f1253c1bb3d92ee096b07ae333ec1f796
Jumen-UX <juan_mendoza@outlook.com>
2026-07-28 21:42:26 -0400

    db: restrict public registry view privileges

---
+++ supabase/migrations/20260729014110_restrict_public_registry_profile_view_privileges.sql
@@ -0,0 +1,4 @@
+revoke all privileges on public.public_ecclesiastical_place_affiliations from anon, authenticated;
+revoke all privileges on public.public_ecclesial_institution_affiliations from anon, authenticated;
+grant select on public.public_ecclesiastical_place_affiliations to anon, authenticated;
+grant select on public.public_ecclesial_institution_affiliations to anon, authenticated;
